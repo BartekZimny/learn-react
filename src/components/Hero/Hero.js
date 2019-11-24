70e41a3f3519a9e460d5283b7f2843ae6e017e6a
Bartłomiej Zimny <bart.zimny@gmail.com>
2019-11-24 22:36:43 +0100

    Add props - first

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,16 @@
 import React from "react";
 import styles from "./Hero.scss";
+import PropTypes from "prop-types";
 
-const Hero = () => (
+const Hero = props => (
   <header className={styles.component}>
-    <h2 className={styles.title}>Things to do</h2>
+    <h2 className={styles.title}>{props.titleText}</h2>
     <img className={styles.image} src="../src/images/space.png"></img>
   </header>
 );
+
+Hero.propTypes = {
+  titleText: PropTypes.node.isRequired
+};
 
 export default Hero;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,23 @@
 import React from "react";
 import styles from "./List.scss";
 import Hero from "../Hero/Hero.js";
+import PropTypes from "prop-types";
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node
+  };
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>
+  };
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} />
+        <div className={styles.description}>{this.props.children}</div>
       </section>
     );
   }
