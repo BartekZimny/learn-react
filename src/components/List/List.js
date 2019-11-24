1e9cded8430a14ee90cee27ab5a2232369a2b427
Bartłomiej Zimny <bart.zimny@gmail.com>
2019-11-24 22:48:02 +0100

    Add Column component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./List.scss";
 import Hero from "../Hero/Hero.js";
+import Column from "../Column/Column.js";
 import PropTypes from "prop-types";
 
 class List extends React.Component {
@@ -19,6 +20,11 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero titleText={this.props.title} imageImg={this.props.image} />
         <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.columns}>
+          <Column title="Animals" />
+          <Column title="Plants" />
+          <Column title="Minerals" />
+        </div>
       </section>
     );
   }
