64965e1658d271954d865af9b02070ff5fc53415
Bartłomiej Zimny <bart.zimny@gmail.com>
2019-12-06 00:29:43 +0100

    Add search component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './App.scss';
 import List from '../List/ListContainer';
 import PropTypes from 'prop-types';
+import Search from '../Search/SearchContainer';
 
 class App extends React.Component {
   static propTypes = {
@@ -17,6 +18,7 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
+        <Search />
         {lists.map(listData => (
           <List key={listData.id} {...listData} />
         ))}
